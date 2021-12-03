941d8de2392c55fa9fc60f5932e91e9ea97b753b
Tony <rafaeltonydavid@yahoo.com>
2021-12-04 00:17:18 +0700

    fix: add defaultvalue for selected tab

--- components/Roadmap/Roadmaptab.tsx
+++ components/Roadmap/Roadmaptab.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import * as Tabs from "@radix-ui/react-tabs";
+import { IdProvider } from "@radix-ui/react-id";
 import { mockFeedback } from "../../mock";
 import { StatusSection } from "../StatusSection";
 
@@ -9,57 +10,59 @@ export const Roadmaptab = () => {
   >("Planned");
 
   return (
-    <Tabs.Root className="md:hidden">
-      <Tabs.List className="flex justify-between py-4 ">
-        <Tabs.TabsTrigger
-          value="Planned"
-          onClick={() => setSelectedTab("Planned")}
-          className={`${
-            selectedTab === "Planned" && "border-cream border-b-4"
-          } cursor-pointer  py-4 `}
-        >
-          Planned
-        </Tabs.TabsTrigger>
-        <Tabs.TabsTrigger
-          value="Live"
-          onClick={() => setSelectedTab("Live")}
-          className={`${
-            selectedTab === "Live" && " border-b-4 border-skyblue"
-          } cursor-pointer py-4 `}
-        >
-          Live
-        </Tabs.TabsTrigger>
-        <Tabs.TabsTrigger
-          value="In-Progress"
-          onClick={() => setSelectedTab("In-Progress")}
-          className={`${
-            selectedTab === "In-Progress" && " border-purple border-b-4"
-          } cursor-pointer py-4 `}
-        >
-          In-Progress
-        </Tabs.TabsTrigger>
-      </Tabs.List>
-      <Tabs.Content value="Planned">
-        <StatusSection
-          title="Planned"
-          description="Ideas prioritized for research"
-          data={mockFeedback.filter((mock) => mock.status === "Planned")}
-        />
-      </Tabs.Content>
-      <Tabs.Content value="Live">
-        <StatusSection
-          title="Live"
-          description="Released features"
-          data={mockFeedback.filter((mock) => mock.status === "Live")}
-        />
-      </Tabs.Content>
-      <Tabs.Content value="In-Progress">
-        <StatusSection
-          title="In-progress"
-          description="Currently being developed"
-          data={mockFeedback.filter((mock) => mock.status === "In-Progress")}
-        />
-      </Tabs.Content>
-    </Tabs.Root>
+    <IdProvider>
+      <Tabs.Root className="md:hidden" defaultValue="Planned">
+        <Tabs.List className="flex justify-between py-4">
+          <Tabs.TabsTrigger
+            value="Planned"
+            onClick={() => setSelectedTab("Planned")}
+            className={`${
+              selectedTab === "Planned" && "border-cream border-b-4"
+            } cursor-pointer  py-4 `}
+          >
+            Planned
+          </Tabs.TabsTrigger>
+          <Tabs.TabsTrigger
+            value="Live"
+            onClick={() => setSelectedTab("Live")}
+            className={`${
+              selectedTab === "Live" && " border-b-4 border-skyblue"
+            } cursor-pointer py-4 `}
+          >
+            Live
+          </Tabs.TabsTrigger>
+          <Tabs.TabsTrigger
+            value="In-Progress"
+            onClick={() => setSelectedTab("In-Progress")}
+            className={`${
+              selectedTab === "In-Progress" && " border-purple border-b-4"
+            } cursor-pointer py-4 `}
+          >
+            In-Progress
+          </Tabs.TabsTrigger>
+        </Tabs.List>
+        <Tabs.Content value="Planned">
+          <StatusSection
+            title="Planned"
+            description="Ideas prioritized for research"
+            data={mockFeedback.filter((mock) => mock.status === "Planned")}
+          />
+        </Tabs.Content>
+        <Tabs.Content value="Live">
+          <StatusSection
+            title="Live"
+            description="Released features"
+            data={mockFeedback.filter((mock) => mock.status === "Live")}
+          />
+        </Tabs.Content>
+        <Tabs.Content value="In-Progress">
+          <StatusSection
+            title="In-progress"
+            description="Currently being developed"
+            data={mockFeedback.filter((mock) => mock.status === "In-Progress")}
+          />
+        </Tabs.Content>
+      </Tabs.Root>
+    </IdProvider>
   );
 };
